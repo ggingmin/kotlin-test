da65f8066245b9348c6a2d9497f247553a1ae982
KYEONGMIN CHO <ggingmin@gmail.com>
2022-12-21 12:36:53 +0900

    first commit

---
+++ src/main/java/com/cloudtype/KotlinGradleTest/KotlinGradleTestApplication.java
@@ -0,0 +1,13 @@
+package com.cloudtype.KotlinGradleTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinGradleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinGradleTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudtype/KotlinGradleTest/KotlinGradleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtype.KotlinGradleTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotlinGradleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
